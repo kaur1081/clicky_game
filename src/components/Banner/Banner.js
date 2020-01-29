1a87ac810f8cc82b2ef46af90e6c01abaa0fbffa
kaur1081 <karishmawalia@gmail.com>
2020-01-29 14:27:03 -0800

    h4 position

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -5,11 +5,8 @@ export const Banner = () => (
   <div className="banner">
     <br></br>
     <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
-    <br></br>
+    
+
     <h4 className='topHead'>
       Click on an Avengers to earn points, but don't click on an image  more than once!
     </h4>
